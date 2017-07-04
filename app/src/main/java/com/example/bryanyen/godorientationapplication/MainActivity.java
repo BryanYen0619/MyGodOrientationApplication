da5be4653f10ec49e62876e887c0db9faafa4ee6
Bryan.Yen <bryan.yen@yfyshop.com>
2017-07-04 10:10:34 +0800

    # 加入sensor切換按鈕

--- app/src/main/java/com/example/bryanyen/godorientationapplication/MainActivity.java
+++ app/src/main/java/com/example/bryanyen/godorientationapplication/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -28,9 +29,11 @@ public class MainActivity extends AppCompatActivity {
 
     private Sensor mAccelerometerSensor;
     private Sensor mMagneticFieldSensor;
+    private Sensor mOrientationSensor;
 
     private float[] accelerometerValues = new float[3];
     private float[] magneticFieldValues = new float[3];
+    private float[] orientationValues = new float[3];
 
     private float currentDegree = 0f;
 
@@ -39,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
 
     private Animation mAlphaInAnimation;
     private Animation mAlphaOutAnimation;
+
+    private boolean isOrientationSensor = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         mAccelerometerSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mMagneticFieldSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        mOrientationSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
 
         LunarCalendar lunarCalendar = new LunarCalendar(new Date());
         godOrientation = DataBaseHelper.getMoneyGodData(getApplication(), lunarCalendar.getLunarMonthOfDay());
@@ -62,20 +68,46 @@ public class MainActivity extends AppCompatActivity {
         whereGoldTextView.setText("財神方位: " + godOrientation);
         mCompassImageView.setDrawingCacheEnabled(true);
 
-        //        mAlphaInAnimation = AnimationUtils.loadAnimation(this, R.anim.alpha_in);
         mAlphaOutAnimation = AnimationUtils.loadAnimation(this, R.anim.alpha_out);
 
-        getCompassOrientation();
+        final Button changeSensorButton = (Button) findViewById(R.id.changeSensorButton);
+        changeSensorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mSensorManager.unregisterListener(sensorEventListener);
+                if (!isOrientationSensor) {
+                    mSensorManager.registerListener(sensorEventListener, mOrientationSensor, SensorManager
+                            .SENSOR_DELAY_NORMAL);
 
+                    changeSensorButton.setText("電子羅盤");
+                    isOrientationSensor = true;
+                } else {
+                    mSensorManager.registerListener(sensorEventListener, mAccelerometerSensor, SensorManager
+                            .SENSOR_DELAY_NORMAL);
+                    mSensorManager.registerListener(sensorEventListener, mMagneticFieldSensor, SensorManager
+                            .SENSOR_DELAY_NORMAL);
+
+                    changeSensorButton.setText("磁場偏移");
+                    isOrientationSensor = false;
+                }
+
+                getCompassOrientation();
+            }
+        });
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mSensorManager.registerListener(sensorEventListener, mAccelerometerSensor, SensorManager
-                .SENSOR_DELAY_NORMAL);
-        mSensorManager.registerListener(sensorEventListener, mMagneticFieldSensor, SensorManager
-                .SENSOR_DELAY_NORMAL);
+        if (isOrientationSensor) {
+            mSensorManager.registerListener(sensorEventListener, mOrientationSensor, SensorManager
+                    .SENSOR_DELAY_NORMAL);
+        } else {
+            mSensorManager.registerListener(sensorEventListener, mAccelerometerSensor, SensorManager
+                    .SENSOR_DELAY_NORMAL);
+            mSensorManager.registerListener(sensorEventListener, mMagneticFieldSensor, SensorManager
+                    .SENSOR_DELAY_NORMAL);
+        }
 
         getCompassOrientation();
     }
@@ -88,11 +120,17 @@ public class MainActivity extends AppCompatActivity {
     private SensorEventListener sensorEventListener = new SensorEventListener() {
         public void onSensorChanged(SensorEvent sensorEvent) {
 
-            if (sensorEvent.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
-                magneticFieldValues = sensorEvent.values;
-            }
-            if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-                accelerometerValues = sensorEvent.values;
+            if (isOrientationSensor) {
+                if (sensorEvent.sensor.getType() == Sensor.TYPE_ORIENTATION) {
+                    orientationValues = sensorEvent.values;
+                }
+            } else {
+                if (sensorEvent.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+                    magneticFieldValues = sensorEvent.values;
+                }
+                if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                    accelerometerValues = sensorEvent.values;
+                }
             }
 
             getCompassOrientation();
@@ -119,76 +157,97 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void getCompassOrientation() {
-        float[] values = new float[3];
-        float[] rotation = new float[9];
-        SensorManager.getRotationMatrix(rotation, null, accelerometerValues, magneticFieldValues);
-        SensorManager.getOrientation(rotation, values);
-
-        // 極座標轉度
-        values[0] = (float) Math.toDegrees(values[0]);
-        //        Log.i(TAG, values[0] + "");
-        //values[1] = (float) Math.toDegrees(values[1]);
-        //values[2] = (float) Math.toDegrees(values[2]);
+    private double getCompassRange(float values) {
+        int angle;
+        if (isOrientationSensor) {
+            angle = 360;    // TYPE_ORIENTATION : 0 ~ 360
+        } else {
+            angle = 0;      // TYPE_ACCELEROMETER : -180 ~ 180
+        }
 
         double maxValue = 0;
         // 判斷座標落在的範圍
-        if (values[0] >= -11.25 && values[0] < 11.25) {
+        if (values >= angle - 11.25 && values < 11.25) {
             compassOrientation = "北";
             maxValue = 0;
-        } else if (values[0] >= 11.25 && values[0] < 33.75) {
+        } else if (values >= 11.25 && values < 33.75) {
             compassOrientation = "東北偏北";
             maxValue = 22.5;
-        } else if (values[0] >= 33.75 && values[0] < 56.25) {
+        } else if (values >= 33.75 && values < 56.25) {
             compassOrientation = "東北";
             maxValue = 45;
-        } else if (values[0] >= 56.25 && values[0] < 78.75) {
+        } else if (values >= 56.25 && values < 78.75) {
             compassOrientation = "東北偏東";
             maxValue = 67.5;
-        } else if (values[0] >= 78.75 && values[0] < 102.25) {
+        } else if (values >= 78.75 && values < 102.25) {
             compassOrientation = "東";
             maxValue = 90;
-        } else if (values[0] >= 101.25 && values[0] < 123.75) {
+        } else if (values >= 101.25 && values < 123.75) {
             compassOrientation = "東南偏東";
             maxValue = 112.5;
-        } else if (values[0] >= 123.75 && values[0] < 146.25) {
+        } else if (values >= 123.75 && values < 146.25) {
             compassOrientation = "東南";
             maxValue = 135;
-        } else if (values[0] >= 146.25 && values[0] < 168.75) {
+        } else if (values >= 146.25 && values < 168.75) {
             compassOrientation = "東南偏南";
             maxValue = 157.5;
-        } else if (values[0] >= 168.75 && values[0] < -168.75) {
+        } else if (values >= 168.75 && values < angle - 168.75) {
             compassOrientation = "南";
             maxValue = 180;
-        } else if (values[0] >= -168.75 && values[0] < -146.25) {
+        } else if (values >= angle - 168.75 && values < angle - 146.25) {
             compassOrientation = "西南偏南";
-            maxValue = -157.5;
-        } else if (values[0] >= -146.25 && values[0] < -123.75) {
+            maxValue = angle - 157.5;
+        } else if (values >= angle - 146.25 && values < angle - 123.75) {
             compassOrientation = "西南";
-            maxValue = -135;
-        } else if (values[0] >= -123.75 && values[0] < -102.25) {
+            maxValue = angle - 135;
+        } else if (values >= angle - 123.75 && values < angle - 102.25) {
             compassOrientation = "西南偏西";
             maxValue = -112.5;
-        } else if (values[0] >= -102.25 && values[0] < -78.75) {
+        } else if (values >= angle - 102.25 && values < angle - 78.75) {
             compassOrientation = "西";
-            maxValue = -90;
-        } else if (values[0] >= -78.75 && values[0] < -56.25) {
+            maxValue = angle - 90;
+        } else if (values >= angle - 78.75 && values < angle - 56.25) {
             compassOrientation = "西北偏西";
-            maxValue = -67.5;
-        } else if (values[0] >= -56.25 && values[0] < -33.75) {
+            maxValue = angle - 67.5;
+        } else if (values >= angle - 56.25 && values < angle - 33.75) {
             compassOrientation = "西北";
-            maxValue = -45;
-        } else if (values[0] >= -33.75 && values[0] < -11.25) {
+            maxValue = angle - 45;
+        } else if (values >= angle - 33.75 && values < angle - 11.25) {
             compassOrientation = "西北偏北";
-            maxValue = -22.5;
+            maxValue = angle - 22.5;
         }
+
+        return maxValue;
+    }
+
+    private void getCompassOrientation() {
+        float values;
+
+        if (isOrientationSensor) {
+            values = orientationValues[0];
+        } else {
+            float[] mValues = new float[3];
+            float[] rotation = new float[9];
+            SensorManager.getRotationMatrix(rotation, null, accelerometerValues, magneticFieldValues);
+            SensorManager.getOrientation(rotation, mValues);
+
+            // 極座標轉度
+            mValues[0] = (float) Math.toDegrees(mValues[0]);
+            //        Log.i(TAG, values[0] + "");
+            //values[1] = (float) Math.toDegrees(values[1]);
+            //values[2] = (float) Math.toDegrees(values[2]);
+
+            values = mValues[0];
+        }
+
+        double maxValue = getCompassRange(values);
 
         mMoneyGodImageView.setVisibility(View.INVISIBLE);
         //        Log.i(TAG, compassOrientation);
         mOrientationTextView.setText(compassOrientation);
 
-        compassImageAnimation(values[0]);
-        godImageShow(compassOrientation, values[0], maxValue);
+        compassImageAnimation(values);
+        godImageShow(compassOrientation, values, maxValue);
     }
 
     private void godImageShow(String compress, float value, double maxValue) {
